ba8a1f8b0357f3d93487958344a266db8c4835d2
No Nils <nonils@tutanota.com>
2020-10-22 14:03:16 -0300

    Initial commit adding swagger configuration

---
+++ src/main/java/com/sebastian/bogado/minesweeper/config/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package com.sebastian.bogado.minesweeper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket productApi() {
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/api/**"))
+				.build()
+				.apiInfo(apiInfo());
+
+	}
+
+	@Bean
+	public SecurityScheme apiKey() {
+		return new ApiKey(HttpHeaders.AUTHORIZATION, "apiKey", "header");
+	}
+
+	@Bean
+	public SecurityScheme apiCookieKey() {
+		return new ApiKey(HttpHeaders.COOKIE, "apiKey", "cookie");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Minesweeper for the payment component")
+				.description("Created by Sebastian Bogado")
+				.termsOfServiceUrl("https://github.com/nonils/minesweeper-API")
+				.license("MIT").version("1.0.0").build();
+	}
+
+}
